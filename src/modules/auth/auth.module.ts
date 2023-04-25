95527230f740152ba22139d1f5edd03c0abdfc32
Daniel <danielhiro13@gmail.com>
2023-04-25 06:57:07 -0300

    feat: implementation of the login logic

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,11 +2,25 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
 import { UsersModule } from '../users/users.module';
-import { PrismaService } from 'src/common/services/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
+import { env } from 'process';
+import { UtilsModule } from 'src/common/utils/utils.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  imports: [UsersModule],
-  providers: [AuthService, PrismaService],
+  imports: [
+    UsersModule,
+    UtilsModule,
+    PassportModule,
+    JwtModule.register({
+      global: true,
+      secret: env.JWT_SECRET,
+      signOptions: { expiresIn: env.EXPIRES_JWT_TOKEN },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
