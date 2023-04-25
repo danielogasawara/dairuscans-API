34f7d26f94c94e2ea2ca5d695791fa1818676eac
Daniel <danielhiro13@gmail.com>
2023-04-25 06:48:25 -0300

    feat: add hashed password comparison

--- src/common/utils/utils.service.ts
+++ src/common/utils/utils.service.ts
@@ -6,4 +6,8 @@ export class UtilsService {
   async encryptPassword(password: string): Promise<string> {
     return await bcrypt.hash(password, 12);
   }
+
+  async comparePassword(password: string, hashInDb: string): Promise<boolean> {
+    return await bcrypt.compare(password, hashInDb);
+  }
 }
