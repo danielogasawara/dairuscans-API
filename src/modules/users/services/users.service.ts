cc73c5c15412c4de705be415c463dd66bfaa1014
Daniel <danielhiro13@gmail.com>
2023-04-25 06:25:10 -0300

    feat(users): add findById and refactor createUser

--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { PrismaService } from 'src/common/services/prisma.service';
 import { UtilsService } from 'src/common/utils/utils.service';
 import { CreateUserDto } from '../dtos/create-user.dto';
@@ -12,18 +12,31 @@ export class UsersService {
 
   async createUser({ name, email, password }: CreateUserDto) {
     const hashedPassword = await this.utils.encryptPassword(password);
-    return this.prisma.users.create({
-      data: {
-        name,
-        email,
-        password: hashedPassword,
-      },
-    });
+    try {
+      this.prisma.users.create({
+        data: {
+          name,
+          email,
+          password: hashedPassword,
+        },
+      });
+
+      return { message: 'Usuário criado com sucesso!' };
+    } catch (error) {
+      console.error(error);
+      return new InternalServerErrorException();
+    }
   }
 
   async findByEmail(email: string) {
     return this.prisma.users.findUnique({
       where: { email },
+    });
+  }
+
+  async findById(id: string) {
+    return this.prisma.users.findUnique({
+      where: { id },
     });
   }
 }
