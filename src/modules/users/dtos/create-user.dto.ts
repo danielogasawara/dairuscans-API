495b03004fbb657c21782bf74315794da1def264
Daniel <danielhiro13@gmail.com>
2023-04-28 18:08:52 -0300

    chore: add ApiProperty in create-user dto

--- src/modules/users/dtos/create-user.dto.ts
+++ src/modules/users/dtos/create-user.dto.ts
@@ -1,12 +1,17 @@
 import { IsEmail, IsString, IsStrongPassword, Length } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty()
   @IsString()
   @Length(3, 255, { message: 'O nome deve conter de 3 a 255 caractéres' })
   name: string;
 
+  @ApiProperty()
   @IsEmail(undefined, { message: 'O formato do e-mail não é válido' })
   email: string;
 
+  @ApiProperty()
   @IsStrongPassword()
   @Length(6, 255, { message: 'A senha deve conter de 6 a 255 caractéres' })
   password: string;
